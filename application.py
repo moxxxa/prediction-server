16c5c38004160a79bf7a358133e140afdb5f52cb
Moxa <mohaxoy98@gmail.com>
2020-07-16 20:32:59 +0200

    prediction improvement

--- application.py
+++ application.py
@@ -1,17 +1,21 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import os
+from os import path
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
-
+model = ""
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/src/uploads"
+DownloadFolder = os.path.dirname(os.path.abspath(__file__)) + "/src/"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 target_size = (96, 96)
-model_path = os.path.dirname(os.path.abspath(__file__)) + "/src/my_model1.keras"
+model_path = os.path.dirname(os.path.abspath(__file__)) + "/src/modelTL2.keras"
+print("model path = " + model_path)
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -23,23 +27,30 @@ def allowed_file(filename):
   # this has changed from the original example because the original did not work for me
     return filename[-3:].lower() in ALLOWED_EXTENSIONS
 
+
+
+def imageToArray(image):
+    image = image.convert('RGB')
+    image = image.resize((200,200))
+    e = np.array(image) / 255
+    e = e.reshape(1, e.shape[0], e.shape[1], 3)
+    return e
+
+
 def predict(filename):
     if filename:
-        img = Image.open(UPLOAD_FOLDER + '/' + filename).convert('RGB').resize(target_size)
+        image = Image.open(UPLOAD_FOLDER + '/' + filename)
         print('[INFO] image loaded successfully')
-        print("[INFO] loading the model ...")
-        model = load_model(model_path)
         print("[INFO] model loaded successfully")
-        print("[INFO] Starting the prediction...")
-        img = np.array(img) / 255.0
-        img = img.reshape((1, img.shape[0], img.shape[1],
-                               img.shape[2]))
-        preds = model.predict(img)
-        print("[INFO] prediction success ", preds)
+
+        imageArray = imageToArray(image)
+        print("[INFO] convertion to array successfully...")
+
+        preds = model.predict(imageArray)
+        # print("preds =" + preds.argmax(axis=-1))
         result = preds.argmax(axis=-1)[0]
         print("[INFO] final result ", result)
         return str(result)
-        # return str(3)
     return 'error'
 
 
@@ -70,5 +81,15 @@ def upload_file():
                 return res_prediction
     return 'error'
 
+def downloadSource():
+    if not path.exists(model_path):
+        print("Download ./prediction/ressource/ File From google drive : ")
+        gdd.download_file_from_google_drive(file_id = '1O3GMPr9kk1hyV2jFNGdtsc9Ne5G_lwK6',
+                                            dest_path = DownloadFolder + 'ressource.zip', unzip=True)
+        os.remove(DownloadFolder + "/ressource.zip")
+
+
 if __name__ == '__main__':
+    downloadSource()
+    model = load_model(model_path)
     app.run()
